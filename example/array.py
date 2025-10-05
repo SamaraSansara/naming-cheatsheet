ec4deac5f4dc5b0ef6b89bf65563e3c0429b098b
Veronika <nikkkbkooo@mail.ru>
2025-10-05 19:43:24 +0300

    IO functions were added.

--- example/array.py
+++ example/array.py
@@ -1,14 +1,23 @@
-arr = list()
-n = int(input("Enter number of elements:"))
-print("Enter elements:")
-i = 0
-while(i < n):
-tmp = int(input(""))
-arr.append(tmp)
-i += 1
-print("Array:")
-i = 0
-while(i < n):
-print(arr[i],end=" ")
-i += 1
-print("")
+NU nano 7.2                           
+
+def read_array():
+    arr = list()
+    n = int(input("Enter number of elements:"))
+    print("Enter elements:")
+    i = 0
+    while i < n:
+        tmp = int(input(""))
+        arr.append(tmp)
+        i += 1
+    return arr
+
+def print_array(arr):
+    print("Array:")
+    for x in arr:
+        print(x, end=" ")
+    print("")
+
+# main
+arr = read_array()
+print_array(arr)
+
