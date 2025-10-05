d7a06a98e9d379df6b65cf5dacaf118a34adafe3
Yurij <yura.abram22811@gmail.com>
2025-10-05 19:29:21 +0300

    Added array size limit (N_MAX = 15) as per task.

--- example/array.py
+++ example/array.py
@@ -1,14 +1,21 @@
+N_MAX = 15
+
 arr = list()
 n = int(input("Enter number of elements:"))
-print("Enter elements:")
-i = 0
-while(i < n):
-tmp = int(input(""))
-arr.append(tmp)
-i += 1
-print("Array:")
-i = 0
-while(i < n):
-print(arr[i],end=" ")
-i += 1
-print("")
+if n > N_MAX:
+    print(f"Error: Maximum {N_MAX} elements allowed.")
+else:
+    print("Enter elements:")
+    i = 0
+    while i < n:
+        tmp = int(input(""))
+        arr.append(tmp)
+        i += 1
+
+    print("Array:")
+    i = 0
+    while i < n:
+        print(arr[i], end=" ")
+        i += 1
+    print("")
+
